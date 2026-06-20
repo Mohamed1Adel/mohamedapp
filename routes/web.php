4bb50982bc6c27988c8d0258d300938bab133b51
mohamed1adel <mohamedqwe542@gmail.com>
2026-06-20 14:15:52 +0300

    l

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,20 @@
 
 use Illuminate\Support\Facades\Route;
 
+use Native\Mobile\Facades\Geolocation;
+
+
 Route::get('/', function () {
     return view('islamic-app');
+});
+
+
+Route::get('/request-location', function () {
+    Geolocation::requestPermissions();
+    return response()->json(['status' => 'requested']);
+});
+
+Route::get('/get-location', function () {
+    Geolocation::getCurrentPosition(true); // true = GPS دقيق
+    return response()->json(['status' => 'requested']);
 });
